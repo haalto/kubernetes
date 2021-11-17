ce97732033ed0c04d88078f781545677aa4fc948
Hannes Aaltonen <aaltonen.hannes@gmail.com>
2021-11-17 21:24:55 +0200

    final logger

--- part1/exercises/log-output/log-output-writer/log-output.go
+++ part1/exercises/log-output/log-output-writer/log-output.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"path/filepath"
+	"strconv"
 	"time"
 
 	"github.com/google/uuid"
@@ -20,12 +22,27 @@ func generateLog() {
 		time.Sleep(5 * time.Second)
 		id := uuid.New()
 		t := time.Now()
+		pongs := 0
+
+		b, err := ioutil.ReadFile(filepath.Join("/", "usr", "src", "app", "files", "pongs.txt"))
+		if err != nil {
+			log.Print(err)
+		} else {
+			p, err := strconv.Atoi(string(b))
+
+			if (err != nil) {
+				log.Print(err)
+				p = 0
+			} else {
+				pongs = p
+			}
+		}
 
 		f, err := os.OpenFile(filepath.Join("/", "usr", "src", "app", "files", "logs.log"), os.O_APPEND|os.O_CREATE|os.O_WRONLY, 0644)
     if err != nil {
         log.Fatal(err)
     }
-    if _, err := f.Write([]byte(t.Format(time.RFC850) + ": " + id.String() + "\n")); err != nil {
+    if _, err := f.Write([]byte(t.Format(time.RFC850) + ": " + id.String() + "\n" + "Ping / Pongs: " + strconv.Itoa(pongs) + "\n")); err != nil {
         log.Fatal(err)
     }
     if err := f.Close(); err != nil {
